3236000efab6986793e67f771e83a90d64855138
Ewan T. Phillips <14805275+ewan-phillips@users.noreply.github.com>
2026-04-16 20:44:15 +0200

    Add copyright and license information
    Added copyright and license information to the file.

--- base_m_noise.py
+++ base_m_noise.py
@@ -1,3 +1,20 @@
+#    Copyright 2026 Ewan Phillips, Benjamin Lindner and Holger Kantz
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the Affero GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+"""
+This script contains the core functions to generate
+simulations and aid with plotting
+"""
+
 import importlib
 
 import numpy as np
